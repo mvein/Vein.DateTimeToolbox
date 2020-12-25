441055016244349510c7558319e59474fb9ab01e
mvein <ekonto@marcinzyla.com>
2020-12-25 21:20:17 +0100

    The solution file together with basic structure has been provided

---
+++ src/Vein.DateTimeToolbox/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vein.DateTimeToolbox.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime Yesterday(this DateTime date)
+        {
+            return date.AddDays(-1);
+        }
+    }
+}
---
+++ tests/Vein.DateTimeToolbox.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using Vein.DateTimeToolbox.Extensions;
+
+namespace Vein.DateTimeToolbox.Tests.Extensions
+{
+    [TestFixture]
+    public class DateTimeExtensionsTests
+    {
+        [Test]
+        public void Yesterday_WhenCalled_ProperDateTimeExpected()
+        {
+            // ARRANGE
+            var dateTime = DateTime.Now;
+            var expected = dateTime.AddDays(-1);
+
+            // ACT
+            var actual = dateTime.Yesterday();
+
+            // ASSERT
+            actual.Should().BeSameDateAs(expected);
+        }
+    }
+}
